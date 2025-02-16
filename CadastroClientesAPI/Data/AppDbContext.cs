e6cf4c6a3f6f3ce9e167023b814cb152a711b922
mtlouzada <matheuslouzadaa@gmail.com>
2025-02-16 13:53:43 -0300

    📂 feat: created a Data folder to establish a connectionwith the SQL database

---
+++ CadastroClientesAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CadastroClientesAPI.Models;
+
+namespace CadastroClientesAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
